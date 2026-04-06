6ab0ba9622e63376c2658161a89f4d31829b5c7a
HandreiJr <juniorhandrei14@gmail.com>
2026-04-06 13:03:00 -0300

    Update ProcessarPedido.java

--- pcmania/util/ProcessarPedido.java
+++ pcmania/util/ProcessarPedido.java
@@ -4,7 +4,6 @@ import pcmania.modelo.Computador;
 
 public class ProcessarPedido {
 
-    // método utilitário estatico que recebe o array de computadores do cliente
     public static void processarPedido(Computador[] computadores, int qtd) {
         System.out.println("\nPedido enviado...");
     }
